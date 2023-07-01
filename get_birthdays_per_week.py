ad28ecd31ffe3e9c09c262effe1b659c58ccf24c
Vityaseek <victor.balas4ov94@gmail.com>
2023-07-01 13:37:08 +0200

    New_commit

--- get_birthdays_per_week.py
+++ get_birthdays_per_week.py
@@ -3,6 +3,13 @@ from faker import Faker
 from faker.providers import profile
 from collections import defaultdict
 
+list1 = [{"name": "Bill", "birthdate": '01.07.2023'}, {"name": 'Jill', "birthdate": "02.07.2023"},
+         {"name": 'Petr', "birthdate": '03.07.2023'}, {"name": 'Petroo', "birthdate": '04.07.2023'}, {
+             "name": 'Lisa', "birthdate": '05.07.2023'},
+         {"name": 'Kisa', "birthdate": '06.07.2023'}, {
+             "name": 'Visa', "birthdate": '07.07.2023'},
+         {"name": 'Bisa', "birthdate": '08.07.2023'}]
+
 
 def get_random_users() -> list:
     list1 = []
@@ -18,36 +25,35 @@ def get_random_users() -> list:
     return list1
 
 
-def get_birthdays_per_week(*args) -> list:
-    next = ''
-    mond = ''
+def get_birthdays_per_week(*args):
+    days_of_week = defaultdict(list)
     curerent_day = datetime.now()
-    args = get_random_users()
     for kee in args:
-        for key, value in kee.items():
-            if key == 'birthdate':
-                if value.weekday() != 5 and value.weekday() != 6:
-                    td = timedelta(weeks=1)
-                    next_week = curerent_day + td
-                    happy = datetime.strftime(next_week, '%A')
-                    if not next:
-                        x = f"{happy} : {kee['name']}"
-                        next += x
-                    else:
-                        next += f', {kee["name"]}'
-                else:
-                    mon = datetime(year=2023, day=3, month=7)
-                    happy_day = datetime.strftime(mon, '%A')
-                    if not mond:
-                        x = f"{happy_day} : {kee['name']}"
-                        mond += x
-                    else:
-                        mond += f', {kee["name"]}'
+        for key in kee:
+            for name, birth in key.items():
+                if name == 'birthdate':
+                    birth = datetime.strptime(birth, '%d.%m.%Y')
+                    if curerent_day.month == birth.month:
+                        td = timedelta(weeks=1)
+                        next_week = curerent_day + td
+                        if birth.day >= curerent_day.day and birth.day <= next_week.day:
+                            if birth.weekday() == 5:
+                                td = timedelta(days=2)
+                                monday = td + birth
+                                happy = datetime.strftime(monday, '%A')
+                                days_of_week[happy].append(key["name"])
+                            elif birth.weekday() == 6:
+                                td = timedelta(days=1)
+                                monday = td + birth
+                                happy = datetime.strftime(monday, '%A')
+                                days_of_week[happy].append(key["name"])
+                            elif birth.weekday() <= 4:
+                                happy = datetime.strftime(birth, "%A")
+                                days_of_week[happy].append(key["name"])
             else:
                 continue
-    print(next)
-    print(mond)
+    print(days_of_week)
 
 
 if __name__ == '__main__':
-    get_birthdays_per_week()
+    get_birthdays_per_week(list1)
